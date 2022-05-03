7dcf1c7d7de41510ed6cf5e76e4f0eb8e5df6583
Lucas Souza <lucasp.sdev@gmail.com>
2022-05-03 19:07:58 -0300

    chore(refactor): resolve build issue refactoring component

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -1,60 +1,36 @@
 import javax.swing.*;
-import javax.swing.text.MaskFormatter;
-import java.text.ParseException;
 
 public class UserRegistration extends JFrame {
-    private JPanel mainContent;
-    private JLabel lbName;
-    private JTextField tfName;
-    private JLabel lbTypeOfStreet;
-    private JTextField tfStreet;
-    private JLabel lbStreet;
-    private JLabel lbStreetNumber;
-    private JTextField tfStreetNumber;
-    private JLabel lbResidencialNumber;
-    private JLabel lbComercialNumber;
-    private JLabel lbPersonalNumber;
-    private JLabel lbCPF;
-    private JLabel lbRG;
-    private JComboBox<String> cbTypeOfStreets;
-    private JFormattedTextField ftfResidencialNumber = null;
-    private JFormattedTextField ftfComercialNumber = null;
-    private JFormattedTextField ftfPersonalNumber = null;
-    private JFormattedTextField ftfCPF = null;
-    private JFormattedTextField ftfRG = null;
+    private JPanel mainPanel;
+    private JTextField jtUserName;
+    private JLabel jlUserName;
+    private JComboBox jcbStreetType;
+    private JLabel jlStreetType;
+    private JLabel jlStreet;
+    private JTextField jtfStreet;
+    private JLabel jlStreetNumber;
+    private JTextField jtfStreetNumber;
+    private JFormattedTextField jftfRecidencialNumber;
+    private JLabel jlRecidencialNumber;
+    private JLabel jlComercialNumber;
+    private JFormattedTextField ftfComercialNumber;
+    private JLabel jlPersonalNumber;
+    private JFormattedTextField ftfPersonalNumber;
+    private JLabel jlCPF;
+    private JFormattedTextField ftfCPF;
+    private JLabel jlRG;
+    private JFormattedTextField ftfRG;
+    
+    public UserRegistration(String title) {
+        super(title);
+    
+        this.setContentPane(mainPanel);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+    }
     
     public static void main(String[] args) {
-        UserRegistration myFrame = new UserRegistration();
-        myFrame.createUIComponents();
-    }
-    
-    public UserRegistration() {
-        setTitle("User Registration");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(500, 500);
-        setVisible(true);
-        createUIComponents();
-    }
-    
-    public void createUIComponents()  {
-        try {
-            MaskFormatter maskResidencialNumber = new MaskFormatter("(##) ####-####");
-             maskResidencialNumber.install(ftfResidencialNumber);
-    
-            MaskFormatter maskComercialNumber = new MaskFormatter("(##) ####-####");
-            maskComercialNumber.install(ftfComercialNumber);
-    
-            MaskFormatter maskPersonalNumber = new MaskFormatter("(##) ####-####");
-            maskPersonalNumber.install(ftfPersonalNumber);
-    
-            MaskFormatter maskCpf = new MaskFormatter("###.###.###-##");
-            maskCpf.install(ftfCPF);
-    
-            MaskFormatter maskRG = new MaskFormatter("##.###.###-#");
-            maskRG.install(ftfRG);
-        } catch (ParseException error) {
-           JOptionPane.showMessageDialog(null, "Erro ao criar máscara", "Erro", JOptionPane.ERROR_MESSAGE);
-        }
-        
+        UserRegistration myFrame = new UserRegistration("User Registration");
+        myFrame.setVisible(true);
     }
 }
